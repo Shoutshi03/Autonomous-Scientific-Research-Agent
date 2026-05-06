3afeabbed2bf2b9331e08c1d85deed18cb62b554
Shoutshi03 <elboualysidina86@gmail.com>
2026-05-06 10:30:59 -1200

    files added

--- src/llm_interface.py
+++ src/llm_interface.py
@@ -1,14 +1,28 @@
 from langchain_openai import ChatOpenAI
 import os
+import streamlit as st
+
 
 class LLMInterface:
-    def __init__(self, provider="openrouter", model_name="deepseek/deepseek-chat", temperature=0):
+    def __init__(
+        self,
+        provider="openrouter",
+        model_name="deepseek/deepseek-chat",
+        temperature=0
+    ):
 
         if provider == "openrouter":
-            api_key = os.getenv("OPENROUTER_API_KEY")
+
+            # priorité aux secrets Streamlit
+            api_key = st.secrets.get(
+                "OPENROUTER_API_KEY",
+                os.getenv("OPENROUTER_API_KEY")
+            )
 
             if not api_key:
-                raise ValueError("OPENROUTER_API_KEY non trouvé dans les variables d'environnement")
+                raise ValueError(
+                    "OPENROUTER_API_KEY introuvable."
+                )
 
             self.llm = ChatOpenAI(
                 model=model_name,
@@ -17,8 +31,9 @@ class LLMInterface:
                 base_url="https://openrouter.ai/api/v1",
                 streaming=True
             )
+
         else:
-            raise ValueError(f"Provider non supporté: {provider}")
+            raise ValueError(f"Provider non supporté : {provider}")
 
     def get_llm(self):
         return self.llm
